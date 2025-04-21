5ff081428d3e1c8a2f9fe72c4c914fe8013a1f3a
ShirleuHuang <137695908+Shirley020116@users.noreply.github.com>
2025-04-21 21:20:00 +0800

    update for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Instagram_practice/', // 👈 非常重要！
   plugins: [
     react(),
     tailwindcss(),
